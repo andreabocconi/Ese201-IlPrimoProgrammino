1d084c36c98b6906a695e4c48d764bd91ef59545
ndbocconi <andreaboc97@gmail.com>
2015-03-03 22:11:11 +0100

    Create sottrazione.c

---
+++ sottrazione.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+main(){
+
+	int numerou,numerod,sottrazione;
+
+	printf("\nInserisci il primo numero: ");
+	scanf("%d",&numerou);
+
+	printf("\nInserisci il secondo numero: ");
+	scanf("%d",&numerod);
+
+	sottrazione=numerou-numerod;
+	printf("\nLa sottrazione vale: %d");
+
+
+printf("\n\n");
+}
