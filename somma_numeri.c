5177acb1054bac1141064519d645399f57d0aedc
ndbocconi <andreaboc97@gmail.com>
2015-03-03 22:08:04 +0100

    Update and rename .gitignore to somma_numeri.c

---
+++ somma_numeri.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+main(){
+
+	int num1,num2,somma;
+
+	printf("\nInserisci il numero 1: ");
+	scanf("%d",&num1);
+
+	printf("\nInserisci il numero 2: ");
+	scanf("%d",&num2);
+
+	somma=num1+num2;
+
+	printf("\nLa somma vale: %d",somma);
+
+printf("\n\n");
+
+}
